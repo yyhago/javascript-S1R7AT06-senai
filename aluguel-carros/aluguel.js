12c7451920d8123ae531fdc55af4bb229c22739f
yyhago <yhago.felipe.teles@gmail.com>
2025-04-08 14:06:01 -0300

    fix code: Arrumei valores com casas decimais

--- aluguel-carros/aluguel.js
+++ aluguel-carros/aluguel.js
@@ -10,8 +10,8 @@ if (carroEscolhido == "Popular") {
     diariaCarro = parseInt(prompt("Quantos dias você ficou com o carro?"));
     contaDiariaCarro = diariaCarro * 90;
 
-    alert(`Você ira gastar em diária: ${contaDiariaCarro} Reais`);
-
+    alert(`Você ira gastar em diária: ${contaDiariaCarro.toFixed(2)} Reais`);
+    
     kmCarroPercorrido = parseFloat(
         prompt("Quantos Kms você percorreu com o carro?")
     );
@@ -19,23 +19,23 @@ if (carroEscolhido == "Popular") {
     if (kmCarroPercorrido <= 100) {
         contaKmCarroPercorrido = kmCarroPercorrido * 0.2;
         alert(
-            `Andando ${kmCarroPercorrido} Km, você terá gasto ${contaKmCarroPercorrido} Reais`
+            `Andando ${kmCarroPercorrido.toFixed(2)} Km, você terá gasto ${contaKmCarroPercorrido.toFixed(2)} Reais`
         );
     } else {
         contaKmCarroPercorrido = kmCarroPercorrido * 0.1;
         alert(
-            `Andando ${kmCarroPercorrido} Km, você terá gasto ${contaKmCarroPercorrido} Reais`
+            `Andando ${kmCarroPercorrido.toFixed(2)} Km, você terá gasto ${contaKmCarroPercorrido.toFixed(2)} Reais`
         );
     }
 
     total = contaDiariaCarro + contaKmCarroPercorrido;
 
-    alert(`No total você ira gastar: ${total} Reais`);
+    alert(`No total você ira gastar: ${total.toFixed(2)} Reais`);
 } else if (carroEscolhido == "Luxo") {
     diariaCarro = parseInt(prompt("Quantos dias você ficou com o carro?"));
     contaDiariaCarro = diariaCarro * 150;
 
-    alert(`Você ira gastar em diária: ${contaDiariaCarro} Reais`);
+    alert(`Você ira gastar em diária: ${contaDiariaCarro.toFixed(2)} Reais`);
 
     kmCarroPercorrido = parseFloat(
         prompt("Quantos Kms você percorreu com o carro?")
@@ -44,16 +44,16 @@ if (carroEscolhido == "Popular") {
     if (kmCarroPercorrido <= 200) {
         contaKmCarroPercorrido = kmCarroPercorrido * 0.3;
         alert(
-            `Andando ${kmCarroPercorrido} Km, você terá gasto ${contaKmCarroPercorrido} Reais`
+            `Andando ${kmCarroPercorrido.toFixed(2)} Km, você terá gasto ${contaKmCarroPercorrido.toFixed(2)} Reais`
         );
     } else {
         contaKmCarroPercorrido = kmCarroPercorrido * 0.25;
         alert(
-            `Andando ${kmCarroPercorrido} Km, você terá gasto ${contaKmCarroPercorrido} Reais`
+            `Andando ${kmCarroPercorrido.toFixed(2)} Km, você terá gasto ${contaKmCarroPercorrido.toFixed(2)} Reais`
         );
     }
 
     total = contaDiariaCarro + contaKmCarroPercorrido;
 
-    alert(`No total você ira gastar: ${total} Reais`);
+    alert(`No total você ira gastar: ${total.toFixed(2)} Reais`);
 }
